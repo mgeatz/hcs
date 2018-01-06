cceeb4c7f015d838b2223023aef2cc232d140ddb
Michael Geatz <mgeatz01@gmail.com>
2018-01-05 21:25:06 -0600

    UI changes

--- views/javascripts/media.js
+++ views/javascripts/media.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	  if (location.pathname === '/media') {
-		      $.ajax({
-		          	url: '/mediaFiles',
-			      	success: function(res) {
-							console.log('res ', res);
-					for(var i =0; i< JSON.parse(res).media.length; i++){
-						
-					}
-				}
-		          });		
-	 }
+  if (location.pathname === '/media') {
+    $.ajax({
+      url: '/mediaFiles',
+      success: function (res) {
+        console.log('res ', res);
+        for (var i = 0; i < JSON.parse(res).media.length; i++) {
+
+        }
+      }
+    });
+  }
 
 });
