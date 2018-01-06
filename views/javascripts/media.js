de83d5b02e7714d2675f3894295b34fde3725af7
Michael Geatz <mgeatz01@gmail.com>
2018-01-05 22:35:00 -0600

    change

--- views/javascripts/media.js
+++ views/javascripts/media.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
   }
 
   var getMedia = function (mediaChoice) {
+    $('#media').html('');
     mediaFiles.forEach(function (value, index) {
       var $media = $('#media'),
         mediaType = value.toLowerCase().substr(value.length-4);
@@ -22,14 +23,12 @@ $(document).ready(function () {
           case 'photos':
             console.log('photos case');
             if (mediaType === '.jpg' || mediaType === '.png') {
-              $media.html('');
               $media.append('<img width="100" src="'+value+'"/>');
             }
             break;
           case 'movies':
             console.log('movies case');
             if (mediaType === '.mov' || mediaType === '.3gp' || mediaType === '.mp4') {
-              $media.html('');
               $media.append('<video width="320" height="240" controls>'+
                 '<source src="'+ value +'" type="video/'+ mediaType.split('.')[1] +'">'+
                 'Your browser does not support the video tag.'+
