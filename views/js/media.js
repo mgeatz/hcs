79e5c38872370b42c1c18dc42ea2d177db116338
Michael Geatz <mgeatz01@gmail.com>
2018-01-14 14:24:25 -0600

    changes

--- views/js/media.js
+++ views/js/media.js
@@ -7,11 +7,6 @@ if (location.pathname === '/media') {
     success: function (res) {
       console.log('res ', res);
       mediaFiles = JSON.parse(res).media;
-
-      if (mediaFiles.length === 0) {
-        $media.text('No Files Found.');
-      }
-
     }
   });
 }
@@ -47,73 +42,77 @@ var getMedia = function (mediaChoice) {
   window.stop();
   $('#media').html('');
 
-  mediaFiles.forEach(function (value, index) {
-    var $media = $('#media'),
-      mediaType = value.toLowerCase().substr(value.length - 4);
+  if (mediaFiles === undefined) {
+    $media.text('No Files Found.');
+  } else {
+    mediaFiles.forEach(function (value, index) {
+      var $media = $('#media'),
+        mediaType = value.toLowerCase().substr(value.length - 4);
 
-    if (mediaType.split('')[0] === '.') {
+      if (mediaType.split('')[0] === '.') {
 
-      switch (mediaChoice) {
-        case 'photos':
-          console.log('photos case');
-          // show tags
-          // /tray1/root/media/Sun-Jan-14-2018-15-BTAG_New_TAGE-1_wizard.jpg
+        switch (mediaChoice) {
+          case 'photos':
+            console.log('photos case');
+            // show tags
+            // /tray1/root/media/Sun-Jan-14-2018-15-BTAG_New_TAGE-1_wizard.jpg
 
-          var splitBTAG = value.split('BTAG_');
+            var splitBTAG = value.split('BTAG_');
 
-          if (mediaType === '.jpg' || mediaType === '.png') {
+            if (mediaType === '.jpg' || mediaType === '.png') {
 
-            if (splitBTAG.length > 1) {
-              // find tag
-              var tagPost = splitBTAG[1],
-                splitTAGE = tagPost.split('_TAGE'),
-                tag = splitTAGE[0];
+              if (splitBTAG.length > 1) {
+                // find tag
+                var tagPost = splitBTAG[1],
+                  splitTAGE = tagPost.split('_TAGE'),
+                  tag = splitTAGE[0];
 
-              if (tag.toLowerCase() === 'new') {
-                $media.append('<button resourceType="photo" class="tag btn btn-warning" id="' + tag + '">' + tag + '</button>');
+                if (tag.toLowerCase() === 'new') {
+                  $media.append('<button resourceType="photo" class="tag btn btn-warning" id="' + tag + '">' + tag + '</button>');
+                } else {
+                  $media.append('<button resourceType="photo" class="tag btn btn-primary" id="' + tag + '">' + tag + '</button>');
+                }
+
               } else {
-                $media.append('<button resourceType="photo" class="tag btn btn-primary" id="' + tag + '">' + tag + '</button>');
-              }
-
-            } else {
-              if ($('#tag_less').length === 0) {
-                $media.append('<button resourceType="photo" class="tag btn btn-danger" id="tag_less">No Tag</button>');
+                if ($('#tag_less').length === 0) {
+                  $media.append('<button resourceType="photo" class="tag btn btn-danger" id="tag_less">No Tag</button>');
+                }
               }
             }
-          }
-          break;
-        case 'movies':
-          console.log('movies case');
-          if (mediaType === '.mov' || mediaType === '.3gp' || mediaType === '.mp4') {
-            $media.append('<a href="' + value + '" target="_blank" style="color: white;">' + value + '</a><br>');
-          }
-          break;
-        case 'docs':
-          console.log('docs case');
-          break;
+            break;
+          case 'movies':
+            console.log('movies case');
+            if (mediaType === '.mov' || mediaType === '.3gp' || mediaType === '.mp4') {
+              $media.append('<a href="' + value + '" target="_blank" style="color: white;">' + value + '</a><br>');
+            }
+            break;
+          case 'docs':
+            console.log('docs case');
+            break;
+        }
+
+        // documents
+        // .pdf .csv
+
+      } else if (mediaType.split('')[0] === 'd') {
+        // check for the following formats
+
+        // documents
+        // .docx
+
+      } else if (mediaType.indexOf('.sh') !== -1) {
+        console.log('script found - ', value, ' target for removal.');
       }
 
-      // documents
-      // .pdf .csv
+    });
 
-    } else if (mediaType.split('')[0] === 'd') {
-      // check for the following formats
-
-      // documents
-      // .docx
-
-    } else if (mediaType.indexOf('.sh') !== -1) {
-      console.log('script found - ', value, ' target for removal.');
-    }
-
-  });
-
-  $('.tag').click(function (event) {
-    var targetId = event.target.id,
-      resourceType = event.target.attributes[0].value;
-    console.log('targetId ', targetId, ' resourceType ', resourceType);
-    fetchResources(resourceType, targetId);
-  });
+    $('.tag').click(function (event) {
+      var targetId = event.target.id,
+        resourceType = event.target.attributes[0].value;
+      console.log('targetId ', targetId, ' resourceType ', resourceType);
+      fetchResources(resourceType, targetId);
+    });
+  }
 
 };
 
