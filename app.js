85291ff987691245aedfb105b23e720f262c19c7
Michael Geatz <mgeatz01@gmail.com>
2018-01-14 11:51:15 -0600

    changes

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.post(api.v1.upload, function (req, res) {
 /**
  * @API Get all files from tray1
  */
-app.get(api.v1.mediaFiles + '/:routePath' + '/:resourceType' + '/:tag', function (req, res) {
+app.get(api.v1.mediaFiles + '/:routePath/:resourceType/:tag', function (req, res) {
   let routePath = req.params.routePath,
     resourceType = req.params.resourceType,
     tag = req.params.tag,
