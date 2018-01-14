37006e02b76f2a88f53c997e4f74ec98f80dd1df
Michael Geatz <mgeatz01@gmail.com>
2018-01-14 14:41:51 -0600

    changes

--- views/js/main.js
+++ views/js/main.js
@@ -1,13 +1,8 @@
-var version = sessionStorage.getItem('version');
-if (version === null) {
-  $.ajax({
-    url: '/api/v1/version',
-    success: function (sha) {
-      console.log('version = ', sha);
-      $('#version').text(JSON.parse(sha).version);
-      sessionStorage.setItem('version', JSON.parse(sha).version);
-    }
-  });
-} else {
-  $('#version').text(sessionStorage.getItem('version'));
-}
+$.ajax({
+  url: '/api/v1/version',
+  success: function (sha) {
+    console.log('version = ', sha);
+    $('#version').text(JSON.parse(sha).version);
+    sessionStorage.setItem('version', JSON.parse(sha).version);
+  }
+});
