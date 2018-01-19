2811ea8ca4acf2d1cdfea6f4f4ec479301175f07
Michael Geatz <mgeatz01@gmail.com>
2018-01-18 22:42:52 -0600

    changes

--- views/js/media.js
+++ views/js/media.js
@@ -43,9 +43,9 @@ var fetchResources = function (resourceType, targetId) {
 
         $media.append('<div style="background: #ddd; border: 5px solid #ddd; border-radius: 2px;' +
           ' display:inline-block; margin: 5px;"><img height="100" src="' + file + '"/><br>' +
-          '<a href="' + file + '" target="_blank">VIEW</a>&nbsp;&nbsp;' +
+          '<a href="' + file + '" target="_blank">OPEN</a>&nbsp;&nbsp;' +
           '<button class="' + file + 'btn btn-default btn-xs" data-toggle="modal" data-target=".edit"' +
-          ' id="' + file + '">EDIT</button><br><sub>Select bulk edit: </sub><input type="checkbox" class="bulk_edit" '+
+          ' id="' + file + '">VIEW & EDIT</button><br><sub>Select bulk edit: </sub><input type="checkbox" class="bulk_edit" '+
           'fileName="' + file + '" style="width:15px;"/></div>');
       });
 
